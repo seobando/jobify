117352ff099c1fdf825554b4f3bb066b0c13ddd0
Sebastian OM <sebastianobandomorales@gmail.com>
2023-02-26 15:06:22 -0500

    Section 22: Shared Layout

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -7,6 +7,8 @@ import {
   SETUP_USER_BEGIN,
   SETUP_USER_SUCCESS,
   SETUP_USER_ERROR,
+  TOGGLE_SIDEBAR,
+  LOGOUT_USER,
 } from "./actions";
 
 const token = localStorage.getItem("token");
@@ -22,6 +24,7 @@ const initialState = {
   token: token,
   userLocation: userLocation || "",
   jobLocation: userLocation || "",
+  showSidebar: false,
 };
 
 const AppContext = React.createContext();
@@ -46,6 +49,12 @@ const AppProvider = ({ children }) => {
     localStorage.setItem("location", JSON.stringify(location));
   };
 
+  const removeUserFromLocalStorage = () => {
+    localStorage.removeItem("user");
+    localStorage.removeItem("token");
+    localStorage.removeItem("location");
+  };
+
   const setupUser = async ({currentUser,endPoint,alertText}) => {
     dispatch({ type: SETUP_USER_BEGIN });
     try {
@@ -67,9 +76,18 @@ const AppProvider = ({ children }) => {
     clearAlert();
   };
 
+  const toggleSidebar = () => {
+    dispatch({ type: TOGGLE_SIDEBAR });
+  };
+
+  const logoutUser = () => {
+    dispatch({ type: LOGOUT_USER });
+    removeUserFromLocalStorage();
+  };   
+
   return (
     <AppContext.Provider
-      value={{ ...state, displayAlert, setupUser }}
+      value={{ ...state, displayAlert, setupUser, toggleSidebar,logoutUser }}
     >
       {children}
     </AppContext.Provider>
