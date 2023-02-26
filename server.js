21384a7056334e5c1ff1f4f7cd767212f05d00f4
Sebastian OM <sebastianobandomorales@gmail.com>
2023-02-26 10:43:06 -0500

    Section 18: Login User Server

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const app = express();
 import dotenv from "dotenv";
 dotenv.config();
 
+import morgan from 'morgan';
+
 // db and authenticateUser
 import connectDB from "./db/connect.js";
 
@@ -15,6 +17,11 @@ import jobsRouter from "./routes/jobsRoutes.js";
 // middleware
 import notFoundMiddleware from "./middleware/not-found.js";
 import errorHandlerMiddleware from "./middleware/error-handler.js";
+
+
+if(process.env.NODE_ENV !== "production") {
+  app.use(morgan("dev"));
+}  
 
 app.use(express.json());
 
