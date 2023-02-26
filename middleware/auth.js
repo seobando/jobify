c6b859d6137e337549b7f51b4374df7993d5ed78
Sebastian OM <sebastianobandomorales@gmail.com>
2023-02-26 16:59:12 -0500

    Section 25: Auth - Server Setup

---
+++ middleware/auth.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+import { UnAuthenticatedError } from "../errors";
+
+const auth = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer")) {
+    throw new UnAuthenticatedError("Autehntication Invalid");
+  }
+  const token = authHeader.split(" ")[1];
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = { userId: payload.userId };
+    next();
+  } catch (error) {
+    throw new UnAuthenticatedError("Autehntication Invalid");
+  }
+};
+
+export default auth;
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-import 'express-async-errors';
+import "express-async-errors";
 import express from "express";
 const app = express();
 
 import dotenv from "dotenv";
 dotenv.config();
 
-import morgan from 'morgan';
+import morgan from "morgan";
 
 // db and authenticateUser
 import connectDB from "./db/connect.js";
@@ -17,24 +17,24 @@ import jobsRouter from "./routes/jobsRoutes.js";
 // middleware
 import notFoundMiddleware from "./middleware/not-found.js";
 import errorHandlerMiddleware from "./middleware/error-handler.js";
+import authenticateUser from "./middleware/auth.js";
 
-
-if(process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV !== "production") {
   app.use(morgan("dev"));
-}  
+}
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.json({msg:"Welcome!"});
+  res.json({ msg: "Welcome!" });
 });
 
 app.get("/api/v1", (req, res) => {
-  res.json({msg:"API"});
+  res.json({ msg: "API" });
 });
 
 app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/jobs", jobsRouter);
+app.use("/api/v1/jobs", authenticateUser, jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
