ea12d812b89186ab598b2e8c45a3c00c479a96aa
Sebastian OM <sebastianobandomorales@gmail.com>
2023-02-26 17:28:51 -0500

    Section 26: Update User - Server

--- controllers/authController.js
+++ controllers/authController.js
@@ -37,16 +37,28 @@ const login = async (req, res) => {
     throw new UnAuthenticatedError("Invalid credentials");
   }
   console.log(user);
-  const isPassword=await user.comparePasswords(password);
+  const isPassword = await user.comparePasswords(password);
   if (!isPassword) {
     throw new UnAuthenticatedError("Invalid credentials");
   }
   const token = user.createJWT();
-  user.password = undefined
-  res.status(StatusCodes.OK).json({user,token,location:user.location});
+  user.password = undefined;
+  res.status(StatusCodes.OK).json({ user, token, location: user.location });
 };
 const updateUser = async (req, res) => {
-  res.send("update user");
+  const { email, name, lastName, location } = req.body;
+  if (!email || !name || !lastName || !location) {
+    throw new BadRequestError("Please provide all values");
+  }
+  const user = await User.findOne({ _id: req.user.userId });
+  user.email = email;
+  user.name = name;
+  user.lastName = lastName;
+  user.location = location;
+
+  await user.save();
+  const token = user.createJWT();
+  res.status(StatusCodes.OK).json({ user, token, location: user.location });
 };
 
 export { register, login, updateUser };
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,18 +1,17 @@
-import jwt from "jsonwebtoken";
-import { UnAuthenticatedError } from "../errors";
+import jwt from 'jsonwebtoken';
+import { UnAuthenticatedError } from '../errors/index.js';
 
 const auth = async (req, res, next) => {
-  const authHeader = req.headers.authorization;
-  if (!authHeader || !authHeader.startsWith("Bearer")) {
-    throw new UnAuthenticatedError("Autehntication Invalid");
+  const token = req.cookies.token;
+  if (!token) {
+    throw new UnAuthenticatedError('Authentication Invalid');
   }
-  const token = authHeader.split(" ")[1];
   try {
     const payload = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = { userId: payload.userId };
+    req.user = { userId: payload.userId, testUser };
     next();
   } catch (error) {
-    throw new UnAuthenticatedError("Autehntication Invalid");
+    throw new UnAuthenticatedError('Authentication Invalid');
   }
 };
 
