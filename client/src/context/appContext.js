dda454b11ec3beb66ce74463783505b957f5d045
Sebastian OM <sebastianobandomorales@gmail.com>
2023-03-01 06:34:52 -0500

    Section 45: Pagination Front-End

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -27,6 +27,7 @@ import {
   SHOW_STATS_BEGIN,
   SHOW_STATS_SUCCESS,
   CLEAR_FILTERS,
+  CHANGE_PAGE
 } from "./actions";
 
 const token = localStorage.getItem("token");
@@ -209,9 +210,9 @@ const AppProvider = ({ children }) => {
   };
 
   const getJobs = async () => {
-    // will add page later
-    const { search, searchStatus, searchType, sort } = state;
-    let url = `/jobs?status=${searchStatus}&jobType=${searchType}&sort=${sort}`;
+    const { page, search, searchStatus, searchType, sort } = state;
+
+    let url = `/jobs?page=${page}&status=${searchStatus}&jobType=${searchType}&sort=${sort}`;
     if (search) {
       url = url + `&search=${search}`;
     }
@@ -228,7 +229,7 @@ const AppProvider = ({ children }) => {
         },
       });
     } catch (error) {
-      // logoutUser()
+      logoutUser();
     }
     clearAlert();
   };
@@ -294,6 +295,9 @@ const AppProvider = ({ children }) => {
     dispatch({ type: CLEAR_FILTERS });
   };
     
+  const changePage = (page) => {
+    dispatch({ type: CHANGE_PAGE, payload: { page } })
+  }
 
   return (
     <AppContext.Provider
@@ -312,7 +316,8 @@ const AppProvider = ({ children }) => {
         deleteJob,
         editJob,
         showStats,
-        clearFilters
+        clearFilters,
+        changePage
       }}
     >
       {children}
