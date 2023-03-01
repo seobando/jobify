2b24ddf3e10ddb5087c0cffc57a060979126ab6c
Sebastian OM <sebastianobandomorales@gmail.com>
2023-03-01 05:43:01 -0500

    Section 41: Monthly Applications - Fron-End

--- client/src/components/ChartsContainer.js
+++ client/src/components/ChartsContainer.js
@@ -1,11 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import BarChart from "./BarChart";
+import AreaChart from "./AreaChart";
+import { useAppContext } from "../context/appContext";
+import Wrapper from "../assets/wrappers/ChartsContainer";
 
-import BarChart from './BarChart';
-import AreaChart from './AreaChart';
-import { useAppContext } from '../context/appContext';
-import Wrapper from '../assets/wrappers/ChartsContainer';
-
-export default function ChartsContainer() {
+const ChartsContainer = () => {
   const [barChart, setBarChart] = useState(true);
   const { monthlyApplications: data } = useAppContext();
 
@@ -13,10 +12,12 @@ export default function ChartsContainer() {
     <Wrapper>
       <h4>Monthly Applications</h4>
 
-      <button type='button' onClick={() => setBarChart(!barChart)}>
-        {barChart ? 'AreaChart' : 'BarChart'}
+      <button type="button" onClick={() => setBarChart(!barChart)}>
+        {barChart ? "AreaChart" : "BarChart"}
       </button>
       {barChart ? <BarChart data={data} /> : <AreaChart data={data} />}
     </Wrapper>
   );
-}
+};
+
+export default ChartsContainer;
